99c4d8ae48a8d8c598ea74dd401c74308a099003
konst857 <perfomansport@gmail.com>
2021-02-28 14:33:23 +0300

    ручной коммит

---
+++ 1.bat
@@ -0,0 +1,8 @@
+cmd /c cd O:   
+cmd /c cd O:\Klients\Экипка\Git_Rep_Ekipka\ 
+git checkout master
+git add "O:\Klients\Экипка\Git_Rep_Ekipka\epf\Формирование док из ексель_a7248f42-e142-11e1-8939-c80aa96c4f88"
+git commit "O:\Klients\Экипка\Git_Rep_Ekipka\epf\Формирование док из ексель_a7248f42-e142-11e1-8939-c80aa96c4f88" -m "тест коммит1"
+@rem git push "https://github.com/konst857/ekip_rep_UT11.git" "origin/feature_sozdNomExcel"
+chcp
+pause
